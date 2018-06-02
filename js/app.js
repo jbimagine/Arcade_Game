4dc8e72a0a12b761a1696bcffc46f714c438bee1
joebates <joebates@jbimagine.com>
2018-06-02 14:40:25 -0700

    update player and enemy positions on screen

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-const Enemy = function () {
+const Enemy = function (x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,31 +26,30 @@ Enemy.prototype.render = function () {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function () {
+const Player = function (x, y) {
     this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
 };
 
-Player.prototype.update = function () {
-
-};
+Player.prototype.update = function () {};
 
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
 };
 
-Player.prototype.handleInput = function (direction) {
-
-};
-
+Player.prototype.handleInput = function (direction) {};
 
 // Now instantiate your objects.
-
+let enemy1 = new Enemy(50, 55);
+let enemy2 = new Enemy(25, 140);
+let enemy3 = new Enemy(50, 225);
+let enemy4 = new Enemy(150, 140);
 // Place all enemy objects in an array called allEnemies
-let allEnemies = [];
+let allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
 // Place the player object in a variable called player
-let player = new Player;
+let player = new Player(200, 375);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
